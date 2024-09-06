1e8e5a89620d68d213c57f3fd6e043f32626e2e7
tpiszar <90849867+tpiszar@users.noreply.github.com>
2024-09-05 23:30:59 -0400

    Testing Stuff Out

--- Assets/HandAnimator.cs
+++ Assets/HandAnimator.cs
@@ -14,6 +14,14 @@ public class HandAnimator : MonoBehaviour
     public float overrideTrig = -1;
     public float overrideGrip = -1;
 
+    public GameObject p;
+    public Transform launchpoint;
+    bool primed;
+    public float force;
+    float shotTime = 0;
+    float shot = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +31,38 @@ public class HandAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         float triggerVal = overrideTrig;
         float gripVal = overrideGrip;
+
+        if (primed)
+        {
+            triggerVal = triggerAction.action.ReadValue<float>();
+
+            if (triggerVal == 0)
+            {
+                primed = false;
+                shotTime = 0.15f;
+            }
+            else
+            {
+                return;
+            }
+        }
+        if (shotTime > 0)
+        {
+            shotTime -= Time.deltaTime;
+            triggerVal = Mathf.Lerp(0, 1, shotTime / 0.25f);
+            gripVal = Mathf.Lerp(0, 1, shotTime / 0.15f);
+            if (shotTime < 0)
+            {
+                shot = 1;
+                GameObject hp = Instantiate(p, launchpoint.position, Quaternion.identity);
+                hp.GetComponent<Rigidbody>().AddForce(launchpoint.forward * force, ForceMode.Impulse);
+            }
+        }
+
         if (triggerVal < 0)
         {
             triggerVal = triggerAction.action.ReadValue<float>();
@@ -32,6 +70,18 @@ public class HandAnimator : MonoBehaviour
         if (gripVal < 0)
         {
             gripVal = gripAction.action.ReadValue<float>();
+        }
+
+        if (shot > 0)
+        {
+            shot -= Time.deltaTime;
+            triggerVal = Mathf.Lerp(triggerVal, 0, shot / 1f);
+            gripVal = Mathf.Lerp(gripVal, 0, shot / 1f);
+        }
+
+        if (!primed && gripVal == 1 && triggerVal == 1)
+        {
+            primed = true;
         }
 
         animator.SetFloat("Trigger", triggerVal);
